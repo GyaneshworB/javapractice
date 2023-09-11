d28935f6819dac0e22089c36b5af51a20ab3e1bf
Gyaneshwor Bagale <Gyaneshwor03@gmail.com>
2023-09-12 04:53:17 +1000

    created four functionality in the assignment
    Started the project with 4 functions given from the assignment.
    1) allowed the user to input assignment name
    2) allowed the user to input marks for the assignment.
    3)report error on the screen to user if there is error in the mark which should be between 0 to 30 only.
    4) prints the assignment name following the student marks;

---
+++ assignment/Studentstatistics.java
@@ -0,0 +1,52 @@
+
+/**
+ * Write a description of class Studentstatistics here.
+ *
+ * @author (Gyaneshwor)
+ * @version (a version number or a date)
+ */
+import java.util.*;
+
+public class Studentstatistics {
+    public static void main(String[] args){
+        
+        //F1:  allows the user to input the assignment name
+    Scanner input = new Scanner(System.in);
+    System.out.print("Enter the assignment name: ");
+    String assessmentName = input.nextLine();
+    
+    //F2 
+    double[] marks = new double[30];
+    int i;
+    for(i=0 ; i< marks.length; i++){
+        double mark;
+        do{
+            System.out.print("Enter the mark: ");
+            mark = input.nextDouble();
+            // F4
+            if (mark <0 || mark>30){
+                System.out.println("Error: The marks should only be between 0 and 30 ");
+            
+            }
+            
+        }while(mark <0 || mark >30);
+        
+        marks[i] = mark;
+        }
+        //f4 do the formating also on your own at home
+        System.out.println("Assessment Name: "+ assessmentName);
+        for(i=0 ; i< marks.length; i++){
+        System.out.print(marks[i]);
+    }
+    
+    
+    
+    
+    
+    }
+    
+    //
+    
+}
+    
+
